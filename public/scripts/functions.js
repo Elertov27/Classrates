5a9ea9ac3d98c1dbb2d87cce63e95dc650602a9c
unknown <elert9@gmail.com>
2019-03-05 20:30:53 +0100

    git

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
     var subscriptionKey = "4b841444d0c146e8be6ea258c596669e";
@@ -32,38 +31,3 @@ function navigateToComments(classId) {
     localStorage.setItem('currentItemId', JSON.stringify(classId));
     window.location.href = 'comments?classId=' + classId;
 }
-=======
-function submitComment(commentControl) {
-    var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "4b841444d0c146e8be6ea258c596669e";
-    var url = "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
-
-    var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
-
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: payload,
-        processData: false,
-        headers: {
-            "Ocp-Apim-Subscription-Key": subscriptionKey,
-            "Content-Type": "application/json"
-        }
-    }).done(function (data) {
-
-        var sentimentRating = Math.round((data.documents[0].score * 100) / 25) + 1;
-
-        var currentItemId = JSON.parse(localStorage.getItem('currentItemId'));
-
-        window.location.href = '/comments?classId=' + currentItemId + '&rating=' + sentimentRating + '&comments=' + comments;
-
-    }).fail(function (xhr, status, err) {
-        alert(err);
-    });
-}
-
-function navigateToComments(classId) {
-    localStorage.setItem('currentItemId', JSON.stringify(classId));
-    window.location.href = 'comments?classId=' + classId;
-}
->>>>>>> f9ee7e379673180c19c8c98766cca7578d60baa4
